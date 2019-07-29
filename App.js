94c0ce8fc387c5b0d2dc70256d5b47e87a7f5037
Mark Vayngrib <mark@tradle.io>
2019-07-29 19:12:55 -0400

    upgrade to RN 0.59.8

--- App.js
+++ App.js
@@ -6,32 +6,103 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
+  AppRegistry,
+  ScrollView,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import base64 from 'base64-js';
+import dgram from 'react-native-udp';
 
-export default class App extends Component<{}> {
+function randomPort() {
+  return Math.random() * 60536 | 0 + 5000 // 60536-65536
+}
+
+// only works for 8-bit chars
+function toByteArray(obj) {
+  var uint = new Uint8Array(obj.length);
+  for (var i = 0, l = obj.length; i < l; i++){
+    uint[i] = obj.charCodeAt(i);
+  }
+
+  return new Uint8Array(uint);
+}
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.updateChatter = this.updateChatter.bind(this);
+    this.state = { chatter: [] };
+  }
+
+  updateChatter(msg) {
+    this.setState({
+        chatter: this.state.chatter.concat([msg])
+    });
+  }
+
+  componentDidMount() {
+    let self = this;
+
+    let a = dgram.createSocket({
+      type: 'udp4',
+      reusePort: 'hello'
+    });
+    let aPort = randomPort();
+    a.bind(aPort, function(err) {
+      if (err) throw err;
+      self.updateChatter('a bound to ' + JSON.stringify(a.address()));
+    })
+
+    let b = dgram.createSocket('udp4');
+    var bPort = randomPort();
+    b.bind(bPort, function(err) {
+      if (err) throw err;
+      self.updateChatter('b bound to ' + JSON.stringify(b.address()));
+    })
+
+    a.on('message', function(data, rinfo) {
+      var str = String.fromCharCode.apply(null, new Uint8Array(data));
+      self.updateChatter('a received echo ' + str + ' ' + JSON.stringify(rinfo));
+      a.close();
+      b.close();
+    })
+
+    b.on('message', function(data, rinfo) {
+      var str = String.fromCharCode.apply(null, new Uint8Array(data));
+      self.updateChatter('b received ' + str + ' ' + JSON.stringify(rinfo));
+
+      // echo back
+      b.send(data, 0, data.length, aPort, '127.0.0.1', function(err) {
+        if (err) throw err
+        self.updateChatter('b echoed data')
+      })
+    })
+
+    b.once('listening', function() {
+      var msg = toByteArray('hello')
+      a.send(msg, 0, msg.length, bPort, '127.0.0.1', function(err) {
+        if (err) throw err
+        self.updateChatter('a sent data')
+      })
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <ScrollView>
+          {this.state.chatter.map((msg, index) => {
+            return (
+              <Text key={index} style={styles.welcome}>
+                {msg}
+              </Text>
+            );
+          })}
+        </ScrollView>
       </View>
     );
   }
